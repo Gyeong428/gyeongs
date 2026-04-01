36b6f83dfbb792f84bd0044994c0549c98eff886
Gyeong428 <hanchit0428@gmail.com>
2026-04-01 03:14:01 +0000

    Fix: Re-uploading for PR

--- sample_script.py
+++ sample_script.py
@@ -1,7 +1,2 @@
-
-def multiply(a, b):
-    return a * b
-
-if __name__ == "__main__":
-    result = multiply(10, 5)
-    print(f"The result of 10 * 5 is: {result}")
+# Final Task Submission
+print('Hello GitHub!')
